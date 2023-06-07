7bdabb193919324107e3ab1424068f497a29286b
dhiarzig <dhia.rzig@gmail.com>
2023-06-07 10:22:17 -0400

    Modified messages in Main to Hello future SEs

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("Hello future software engineers!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
